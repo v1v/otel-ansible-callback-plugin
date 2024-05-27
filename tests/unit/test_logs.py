60ab6d9cc26403e33a95a44601eb96d815b241c3
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-05-27 23:52:05 +0200

    disable json attribute

---
+++ tests/unit/test_logs.py
@@ -0,0 +1,40 @@
+import pytest
+import json
+
+
+def test_playbook_with_long_output(helpers):
+    """test a basic playbook"""
+
+    playbook = "playbook.yml"
+    ## Given a playbook
+    with open(playbook, 'w', encoding="utf-8") as f:
+        f.write("""---
+- name: playbook
+  hosts: localhost
+  connection: local
+  tasks:
+    - name: Get build data
+      ansible.builtin.uri:
+        url: "https://artifacts-api.elastic.co/v1/versions"
+        validate_certs: no
+""")
+
+    ## When running the ansible playbook with the plugin
+    span_list = helpers.run_ansible()
+
+    ## Then
+    for span in span_list:
+        if span["name"] == "Gathering Facts":
+            helpers.assertGatheringFacts(span)
+        if span["name"] == "Get build data":
+            helpers.assertCommonSpan(span)
+            assert span["attributes"]["ansible.task.module"] == "ansible.builtin.uri"
+            assert len(span["events"]) == 1
+            assert len(span["attributes"]["ansible.task.args.name"]) == 2
+            assert len(span["attributes"]["ansible.task.args.value"]) == 2
+            print('....')
+            assert "json" not in json.loads(span["events"][0]["name"])
+        if span["name"] == playbook:
+            helpers.assertPlaybook(span)
+    assert len(span_list) == 3
+
